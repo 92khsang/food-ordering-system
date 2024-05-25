5455879e7095a6e3b67b135040896c10a46fe3ce
Hayes <92khsang@gmail.com>
2024-05-26 01:22:21 +0900

    Section 03/Lecture 23 주문 추적 명령 핸들러 구현

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -2,15 +2,33 @@ package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
 import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
+import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
+import com.food.ordering.system.order.service.domain.ports.out.repository.OrderRepository;
+import com.food.ordering.system.order.service.domain.vo.TrackingId;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class OrderTrackCommandHandler {
 
-    TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
-        return null;
+    private final OrderDataMapper orderDataMapper;
+
+    private final OrderRepository orderRepository;
+
+    @Transactional(readOnly = true)
+    public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
+        Order order = orderRepository.findByTrackingId(new TrackingId(trackOrderQuery.getOrderTrackingId()))
+                .orElseThrow(() -> {
+                    log.warn("Could not find order with tracking id: {}", trackOrderQuery.getOrderTrackingId());
+                    return new OrderNotFoundException("Could not find order with tracking id: " + trackOrderQuery.getOrderTrackingId());
+                });
+        return orderDataMapper.orderToTrackOrderResponse(order);
     }
 
 }
