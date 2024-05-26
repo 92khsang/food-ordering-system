a7215868cd8642edf687f8d70ac6f41127d0b8d4
Hayes <92khsang@gmail.com>
2024-05-26 14:38:37 +0900

    Section 05/Lecture 33 애플리케이션 모듈: 컨트롤러 어드바이스 추가하기

---
+++ common/common-application/src/main/java/com/food/ordering/system/application/handler/ErrorDTO.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.application.handler;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@Builder
+@RequiredArgsConstructor
+public class ErrorDTO {
+    private final String code;
+    private final String message;
+}
